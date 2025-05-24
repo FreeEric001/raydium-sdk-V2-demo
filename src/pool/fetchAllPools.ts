1acffefd403a40202fab5dc4a22bdb6b93653500
eric <freeeric001@gmail.com>
2025-05-24 11:53:25 +0800

    .

--- src/pool/fetchAllPools.ts
+++ src/pool/fetchAllPools.ts
@@ -1,6 +1,7 @@
-import { Connection, PublicKey } from '@solana/web3.js'
+import { Connection, PublicKey,clusterApiUrl } from '@solana/web3.js'
 import {
   ALL_PROGRAM_ID,
+  DEVNET_PROGRAM_ID,
   liquidityStateV4Layout,
   PoolInfoLayout,
   CpmmPoolInfoLayout,
@@ -8,11 +9,24 @@ import {
   publicKey,
 } from '@raydium-io/raydium-sdk-v2'
 
-const connection = new Connection('rpc url')
+//const connection = new Connection('rpc url')
+export const connection = new Connection(clusterApiUrl('devnet'))
 
 const getAmmPoolInfo = async (poolId: PublicKey) => {
   const data = await connection.getAccountInfo(poolId)
   if (!data) throw new Error(`pool not found: ${poolId.toBase58()}`)
+  //let rsp = liquidityStateV4Layout.decode(data.data)
+  
+  let rsp = CpmmPoolInfoLayout.decode(data.data)
+  console.log(rsp);
+
+  // const cpmmPools: (ReturnType<typeof CpmmPoolInfoLayout.decode> & { poolId: PublicKey })[] = []
+  // cpmmPools.push({
+  //       poolId: new PublicKey('4hjnV6oaPa62yHfRGc2PD9gJL3HCecU2KpyLGhrULuPS'),
+  //       ...CpmmPoolInfoLayout.decode(data.data),
+  //     })
+  //console.log(cpmmPools);
+
   return liquidityStateV4Layout.decode(data.data)
 }
 
@@ -31,10 +45,10 @@ const getMultipleAmmPoolInfo = async (poolIdList: PublicKey[]) => {
 async function fetchAllPools() {
   // since amm pool data at least > 1GB might cause error
   // let's limit data size and get poolId/baseMint/quoteMint first
-  const layoutAmm = struct([publicKey('baseMint'), publicKey('quoteMint')])
+  const layoutAmm = struct([publicKey('49j9SgJWYidjtCyjroEzfa9NCyEoY9Zjmzmhxy6PeQyK'), publicKey('6BJwba9NyE3FJttj5PLTYmbeTzA6wMi44xaX4k6QqKZ5')])
   const ammPools: (ReturnType<typeof layoutAmm.decode> & { poolId: PublicKey })[] = []
 
-  console.log('amm fetching...')
+  /*console.log('amm fetching...')
   const ammPoolsData = await connection.getProgramAccounts(ALL_PROGRAM_ID.AMM_V4, {
     filters: [{ dataSize: liquidityStateV4Layout.span }],
     dataSlice: { offset: liquidityStateV4Layout.offsetOf('baseMint'), length: 64 },
@@ -46,7 +60,7 @@ async function fetchAllPools() {
       poolId: a.pubkey,
       ...layoutAmm.decode(a.account.data),
     })
-  })
+  })*/
 
   // after get all amm pools id, we can fetch amm pool info one by one or by group separately
 
@@ -54,7 +68,7 @@ async function fetchAllPools() {
   // e.g. 200 per group  getMultipleAmmPoolInfo(ammPools.slice(0, 100).map((a) => a.poolId))
 
   const clmmPools: (ReturnType<typeof PoolInfoLayout.decode> & { poolId: PublicKey })[] = []
-  console.log('clmm fetching...')
+  /*console.log('clmm fetching...')
   const clmmPoolsData = await connection.getProgramAccounts(ALL_PROGRAM_ID.CLMM_PROGRAM_ID, {
     filters: [{ dataSize: PoolInfoLayout.span }],
   })
@@ -65,26 +79,40 @@ async function fetchAllPools() {
       poolId: c.pubkey,
       ...PoolInfoLayout.decode(c.account.data),
     })
-  })
+  })*/
 
   const cpmmPools: (ReturnType<typeof CpmmPoolInfoLayout.decode> & { poolId: PublicKey })[] = []
   console.log('cpmm fetching...')
-  const cpmmPoolsData = await connection.getProgramAccounts(ALL_PROGRAM_ID.CREATE_CPMM_POOL_PROGRAM, {
+  const cpmmPoolsData = await connection.getProgramAccounts(DEVNET_PROGRAM_ID.CREATE_CPMM_POOL_PROGRAM, {
     filters: [{ dataSize: CpmmPoolInfoLayout.span }],
   })
+  /*const targetPublicKey = new PublicKey('4hjnV6oaPa62yHfRGc2PD9gJL3HCecU2KpyLGhrULuPS').toBase58();
+  const cpmmPoolsData = await connection.getProgramAccounts(DEVNET_PROGRAM_ID.CREATE_CPMM_POOL_PROGRAM, {
+    filters: [{ 
+        memcmp: {
+            offset: 0, // 数据偏移量，从哪个字节开始比较
+            bytes: targetPublicKey, // 目标公钥字符串，需要是Base58编码
+        },
+      }],
+  })*/
   cpmmPoolsData.forEach((c) => {
-    cpmmPools.push({
-      poolId: c.pubkey,
-      ...CpmmPoolInfoLayout.decode(c.account.data),
-    })
+    //console.log('--',c.pubkey);
+    if(c.pubkey.toBase58() == '4hjnV6oaPa62yHfRGc2PD9gJL3HCecU2KpyLGhrULuPS')
+    {
+      cpmmPools.push({
+        poolId: c.pubkey,
+        ...CpmmPoolInfoLayout.decode(c.account.data),
+      })
+    }
   })
   console.log('cpmm fetch done')
 
   console.log(ammPools.length, {
-    amm: ammPools,
-    clmmPools: clmmPools,
+    //amm: ammPools,
+    //clmmPools: clmmPools,
     cpmmPools,
   })
 }
 
 fetchAllPools()
+//getAmmPoolInfo(new PublicKey('4hjnV6oaPa62yHfRGc2PD9gJL3HCecU2KpyLGhrULuPS'))
